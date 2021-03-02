d9bfbe0eb9e9f2f5de2b453a977ae73d19b3d010
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-03-02 15:35:43 +1300

    final commit

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,17 @@
+// Components
+import CharacterItem from './CharacterItem';
+import Spinner from '../ui/Spinner';
+
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <section className='cards'>
+      {items.map((item) => (
+        <CharacterItem key={item.char_id} item={item}></CharacterItem>
+      ))}
+    </section>
+  );
+};
+
+export default CharacterGrid;
